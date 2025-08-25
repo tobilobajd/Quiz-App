0d6d8b48837a7f6a7cefc6c572640e48b39a62b1
Japhet <daisioluwatobiloba441@gmail.com>
2025-08-25 15:15:08 +0100

    question section and backend

--- script.js
+++ script.js
@@ -10,4 +10,58 @@ const questions = [
 			{ text: 'Giraffe', correct: false },
 		],
 	},
+	{
+		question: 'Which is the smallest country in the world?',
+		answers: [
+			{ text: 'Vatican City', correct: true },
+			{ text: 'Monaco', correct: false },
+			{ text: 'Nauru', correct: false },
+			{ text: 'Tuvalu', correct: false },
+		],
+	},
+	{
+		question: 'Which is the largest desert in the world?',
+		answers: [
+			{ text: 'Sahara', correct: true },
+			{ text: 'Arabian', correct: false },
+			{ text: 'Gobi', correct: false },
+			{ text: 'Kalahari', correct: false },
+		],
+	},
+	{
+		question: 'Which is the smallest continent in the world?',
+		answers: [
+			{ text: 'Australia', correct: true },
+			{ text: 'Europe', correct: false },
+			{ text: 'Antarctica', correct: false },
+			{ text: 'South America', correct: false },
+		],
+	},
 ];
+
+const questionElement = document.getElementById('question');
+const answerButton = document.getElementById('answer-buttons');
+const nextButton = document.getElementById('next-button');
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+	currentQuestionIndex = 0;
+	score = 0;
+	nextButton.InnerHTML = 'Next';
+	showQuestion();
+}
+
+function showQuestion() {
+	let currentQuestion = questions[currentQuestionIndex];
+	let questionNo = currentQuestionIndex + 1;
+	questionElement.innerHTML = questionNo + '. ' + currentQuestion.question;
+
+	currentQuestion.answers.forEach((answer) => {
+		const button = document.createElement('button');
+		button.innerHTML = answer.text;
+		button.classList.add('btn');
+		answerButton.appendChild(button);
+	});
+}
